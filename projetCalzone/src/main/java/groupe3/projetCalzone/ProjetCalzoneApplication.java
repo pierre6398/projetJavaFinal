4b800a86ca2826b89de6d1d7d1fb520dc8a7488a
Pierre6398 <pierre.cornemillot@gmail.com>
2024-05-02 12:37:13 +0200

    création du projet

---
+++ projetCalzone/src/main/java/groupe3/projetCalzone/ProjetCalzoneApplication.java
@@ -0,0 +1,13 @@
+package groupe3.projetCalzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetCalzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetCalzoneApplication.class, args);
+	}
+
+}
---
+++ projetCalzone/src/test/java/groupe3/projetCalzone/ProjetCalzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package groupe3.projetCalzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetCalzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
